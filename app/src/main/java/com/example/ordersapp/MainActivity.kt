26aef82bff6fad09f4ef734075cf80fbe3a5742c
tamerlankayak <tamerlan_94@mail.ru>
2023-04-25 13:35:04 +0400

    new

--- app/src/main/java/com/example/ordersapp/MainActivity.kt
+++ app/src/main/java/com/example/ordersapp/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import com.example.ordersapp.ui.theme.OrdersAppTheme
-
+//test
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
